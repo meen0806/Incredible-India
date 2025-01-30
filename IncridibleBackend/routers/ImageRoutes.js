2b5420a5f6e86a2b3789a2dd2a4362d7bb41e73e
meen0806 <meenu.yd05@gmail.com>
2025-01-30 16:50:30 +0530

    Delete IncridibleBackend directory

--- IncridibleBackend/routers/ImageRoutes.js
+++
@@ -1,6 +0,0 @@
-import express from "express";
-import { register } from "../controllers/ImageUpload.js";
-import { upload } from "../middlewares/upload.js";
-export const UploadRouter = express.Router();
-
-UploadRouter.post("/upload", upload.single("picture"), register);
--- IncridibleBackend/routers/authRoutes.js
+++
@@ -1,9 +0,0 @@
-import express from "express";
-import { register, login } from "../controllers/authController.js";
-export const authRouter = express.Router();
-
-// @route POST /api/auth/register
-authRouter.post("/Register", register);
-
-// @route POST /api/auth/login
-authRouter.post("/Login", login);
